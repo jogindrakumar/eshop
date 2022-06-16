b6b5e6929c58a8df635e047fc24d10d3eef8de3e
jogindrakumar <joginder9868@outlook.com>
2022-06-16 20:47:28 +0530

    modified:   resources/views/frontend/product/index.blade.php 	modified:   resources/views/frontend/product/singleproductview.blade.php

--- resources/views/frontend/product/index.blade.php
+++ resources/views/frontend/product/index.blade.php
@@ -6,6 +6,12 @@
 @endsection
 
 @section('content')
+<div class="py-3 mb-4 shadow-sm bg-warning border-top">
+
+    <div class="container">
+        <h6 class="mb-0">Collection / {{ $category->name }} </h6>
+    </div>
+</div>
 <div class="py-5">
     <div class="container">
         <div class="row">
--- resources/views/frontend/product/singleproductview.blade.php
+++ resources/views/frontend/product/singleproductview.blade.php
@@ -10,7 +10,7 @@
 <div class="py-3 mb-4 shadow-sm bg-warning border-top">
 
     <div class="container">
-        <h6 class="mb-0">Collection / Category Name / Product Name</h6>
+        <h6 class="mb-0">Collection / {{ $products->category->name }}/ {{ $products->name }}</h6>
     </div>
 </div>
 
@@ -23,14 +23,42 @@
             <div class="col-md-8">
                 <h2 class="mb-0">
                     {{ $products->name }}
-                    <label style="font-size: 16px;" class="float-end badge bg-danger trending-tag" for="">Trending</label>
+
+                    @if ( $products->trending == '1')
+                    <label style="font-size: 16px;" class="float-end badge bg-danger trending-tag" for="">
+                    Trending
+                    </label>
+
+                    @endif
+                    
                 </h2>
                 <hr>
                 <label for="" class="me-3">Original Price : <s> Rs {{ $products->original_price }}</s></label>
-                <label for="" class="fw-bold">Selling Price : <s> Rs {{ $products->original_price }}</s></label>
+                <label for="" class="fw-bold">Selling Price :  Rs {{ $products->selling_price }}</label>
                 <p class="mt-3">
                     {{ $products->small_description }}
                 </p>
+                <hr>
+                @if ($products->qty > 0)
+                <label for="" class="badge bg-success">In Stock</label>
+                  @else  
+                  <label for="" class="badge bg-danger">Out of Stock</label>
+                @endif
+                <div class="row mt-2">
+                    <div class="col-md-2">
+                        <label for="Quantity">Quantity</label>
+                    <div class="input-group text-center mb-3">
+                        <span class="input-group-text">-</span>
+                        <input type="text" name="quantity" value="1" class="form-control">
+                        <span class="input-group-text">+</span>
+                    </div>
+                    </div>
+                    <div class="col-md-10">
+                        <br>
+                        <button class="btn btn-success me-3 float-start" type="button">Add to Wishlist</button>
+                        <button class="btn btn-primary me-3 float-start" type="button">Add to Cart</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
