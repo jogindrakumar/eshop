b032c6c9cc9ad854ddb7ae7c845820d8b5e309ed
jogindrakumar <joginder9868@outlook.com>
2022-06-17 10:40:39 +0530

    modified:   resources/views/frontend/product/singleproductview.blade.php

--- resources/views/frontend/product/singleproductview.blade.php
+++ resources/views/frontend/product/singleproductview.blade.php
@@ -47,10 +47,10 @@
                 <div class="row mt-2">
                     <div class="col-md-2">
                         <label for="Quantity">Quantity</label>
-                    <div class="input-group text-center mb-3">
-                        <span class="input-group-text">-</span>
-                        <input type="text" name="quantity" value="1" class="form-control">
-                        <span class="input-group-text">+</span>
+                    <div class="input-group text-center mb-3" style="width: 130px;">
+                        <button class="input-group-text decrement-btn">-</button>
+                        <input type="text" name="quantity"  class="form-control qty-input text-center" value="1">
+                        <button class="input-group-text increment-btn">+</button>
                     </div>
                     </div>
                     <div class="col-md-10">
@@ -65,4 +65,37 @@
 </div>
 
 
+@endsection
+
+
+@section('scripts')
+<script>
+
+    $(document).ready(function() {
+        $('.increment-btn').click(function(e){
+            e.preventDefault();
+
+            var inc_value = $('.qty-input').val();
+            var value = parseInt(inc_value, 10);
+            value = isNaN(value) ? 0 : value;
+            if(value < 10){
+                value ++;
+                $('.qty-input').val(value);
+            }
+        });
+
+        $('.decrement-btn').click(function(e){
+            e.preventDefault();
+
+            var inc_value = $('.qty-input').val();
+            var value = parseInt(inc_value, 10);
+            value = isNaN(value) ? 0 : value;
+            if(value > 1){
+                value --;
+                $('.qty-input').val(value);
+            }
+        });
+
+    });
+</script>
 @endsection
